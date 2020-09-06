e1166479367f4852ad92019ccd2b089269c702df
tancgo <542112893@qq.com>
2020-09-06 21:42:44 +0800

    书籍布局

--- pages/book/book.js
+++ pages/book/book.js
@@ -1,23 +1,74 @@
-// pages/book/book.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
+import { getHotList, getMyBookCount } from '../../api/book.js'
+Page({
 
   /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
+    books: [],
+    searching: false,
+    more: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: async function (options) {
+    const books = await getHotList()
+
+    if (books) {
+      this.setData({
+        books
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
 
   },
 
   /**
-   * 组件的方法列表
+   * 生命周期函数--监听页面显示
    */
-  methods: {
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
 
   }
 })
